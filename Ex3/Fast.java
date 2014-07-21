e1a20c58d5d5d3ebe73c5ae29faa7841c27f549b
tkhubert <tkhubert@gmail.com>
2014-07-21 18:11:51 +0100

    binary search for performance improvement

--- Ex3/Fast.java
+++ Ex3/Fast.java
@@ -2,6 +2,23 @@ import java.util.Arrays;
 
 public class Fast
 {
+    private static int binarySearch(Point[] array, Point origin, double slope)
+    {   
+        int lo = 0;
+        int hi = array.length - 1;
+        while (lo <= hi) 
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (slope < origin.slopeTo(array[mid]))
+                hi = mid - 1;
+            else if (slope > origin.slopeTo(array[mid]))
+                lo = mid + 1;
+            else 
+                return mid;
+        }
+        return -1;     
+    }
+    //
     public static void main(String[] args)
     {
         StdDraw.setXscale(0, 32768);
@@ -25,25 +42,32 @@ public class Fast
         }
         
         Arrays.sort(points);   
-        
+       
+          
         for (int i=0; i<N; ++i)
         {
             Point p_ori = points[i];
             
             Point[] sameSlopePoints = new Point[N-i];
             sameSlopePoints[0] = p_ori;
-                    
-            Point[] tmpPoints = new Point[N-1-i];
+            
+            Point[] fwdPoints = new Point[N-i-1];
+            Point[] bckPoints = new Point[i];
+            
+            for (int j=0; j<i; ++j)
+                bckPoints[j] = points[j];
             for (int j=i+1; j<N; ++j)
-                tmpPoints[j-i-1] = points[j];
-            Arrays.sort(tmpPoints, p_ori.SLOPE_ORDER);
+                fwdPoints[j-i-1] = points[j];
+            
+            Arrays.sort(bckPoints, p_ori.SLOPE_ORDER);
+            Arrays.sort(fwdPoints, p_ori.SLOPE_ORDER);
             
             int    count = 1;
-            double currentSlope =  N-1-i>0 ? p_ori.slopeTo(tmpPoints[0]) : 0;
+            double currentSlope =  N-1-i>0 ? p_ori.slopeTo(fwdPoints[0]) : 0;
             
             for (int j=0; j<N-1-i; ++j)
             {
-                Point pj = tmpPoints[j];
+                Point pj = fwdPoints[j];
                 double slope = p_ori.slopeTo(pj);
                 
                 if (slope==currentSlope)
@@ -56,18 +80,9 @@ public class Fast
                 {
                     if (count>=4)
                     {
-                        boolean notYetSeen = true;
-                        for (int k=0; k<i; ++k)
-                        {
-                            double oldSlope = points[k].slopeTo(p_ori);
-                            if (oldSlope==currentSlope)
-                            {
-                                notYetSeen = false;
-                                break;
-                            } 
-                        }
-                        
-                        if (notYetSeen)
+                        int hasAlreadySeen = binarySearch(bckPoints, p_ori, currentSlope);
+                                                
+                        if (hasAlreadySeen==-1)
                         {
                             Arrays.sort(sameSlopePoints, 0, count);
                             sameSlopePoints[0].drawTo(sameSlopePoints[count-1]);
@@ -85,7 +100,6 @@ public class Fast
                     currentSlope = slope;
                 }
             }
-            
         }
                 
         // display to screen all at once
